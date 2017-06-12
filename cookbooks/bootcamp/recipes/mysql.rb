b723594ae99536e3a97c695d42ef13a2b124cfbe
Peter <peter.filbin@kibocommerce.com>
2017-06-12 15:08:14 -0500

    added restart command for mysqld

--- cookbooks/bootcamp/recipes/mysql.rb
+++ cookbooks/bootcamp/recipes/mysql.rb
@@ -1,9 +1,3 @@
-package "mysql" do
+package "mysql-server" do
 	action :install
 end
-
-service 'mysql' do
-	service_name 'mysql'
-       	action [:start, :enable]
-end
-
--- cookbooks/bootcamp/recipes/php.rb
+++ cookbooks/bootcamp/recipes/php.rb
@@ -1,8 +1,18 @@
-package "php5" do
+package "php" do
 	action :install
 end
 
 file 'default www' do
 	path '/var/www/html/index.php'
 	content "<?php phpinfo(); ?>"
+end
+
+service 'apache' do
+        service_name 'httpd'
+        action [:restart]
+end
+
+service 'mysql-server' do
+	service_name 'mysqld'
+	action [:restart, :enable]
 end
